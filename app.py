f7837cb967397bcad6d2b07ae0e58f448189736b
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-06-30 19:41:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.title("GSD Dashboard")
 
 st.sidebar.header("Time Period")
+st.write("Year")
+st.write("Quarter")
+st.write(("Month")
+st.sidebar.header("Client/Department")
